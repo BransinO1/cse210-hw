b77a1cef0fe0077d2fa27780a4e5cdde3759be9e
BransinO1 <corbinmeacham65@gmail.com>
2024-03-27 16:06:37 -0600

    w05 polymorphism activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,91 @@
+// Week 05 Learning Activity - Creating Shapes using Polymorphism
+
 using System;
+using System.Collections.Generic;
+
+class Shape
+{
+    protected string color;
+
+    public Shape(string color)
+    {
+        this.color = color;
+    }
+
+    public string Color
+    {
+        get { return color; }
+        set { color = value; }
+    }
+
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
+
+class Square : Shape
+{
+    private double side;
+
+    public Square(string color, double side) : base(color)
+    {
+        this.side = side;
+    }
+
+    public override double GetArea()
+    {
+        return side * side;
+    }
+}
+
+class Rectangle : Shape
+{
+    private double length;
+    private double width;
+
+    public Rectangle(string color, double length, double width) : base(color)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    public override double GetArea()
+    {
+        return length * width;
+    }
+}
+
+class Circle : Shape
+{
+    private double radius;
+
+    public Circle(string color, double radius) : base(color)
+    {
+        this.radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * radius * radius;
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.Color}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
     }
 }
